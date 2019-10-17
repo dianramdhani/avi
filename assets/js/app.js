0d3292159d57ffa56cc23aa8ca76b1d858c43ca6
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-10-17 16:51:52 +0700

    init logo, update quality-verification: update view

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,38 @@
-window.app = angular.module('AVI', []);
+window.app = angular.module('AVI', ['ui.router', 'ngCookies']);
+
+// RUN
+(function () {
+    'use strict';
+
+    window.app
+        .run(Run);
+
+    Run.$inject = ['$state', '$rootScope', '$cookies', '$http'];
+    function Run($state, $rootScope, $cookies, $http) {
+        // $rootScope['global'] = {
+        //     user: angular.fromJson($cookies.get('user')),
+        //     menu: angular.fromJson($cookies.get('menu'))
+        // } || {};
+        // if (typeof $rootScope.global.user === 'undefined') {
+        //     $state.go('login');
+        // } else {
+        //     // $http.defaults.headers.common = { token: $rootScope.global.user.token };
+        //     $state.go('etl');
+        // }
+        $state.go('user');
+    }
+})();
+
+// ROUTES
+require('../routes/app.route');
+require('../routes/user.route');
+
+// DIRECTIVES
+require('../directives/tr-files.directive');
+
+// COMPONENTS
+require('../components/tr-wrapper/tr-wrapper');
 
 // VIEWS
+require('../views/user-container/user-container');
 require('../views/quality-verification/quality-verification');
