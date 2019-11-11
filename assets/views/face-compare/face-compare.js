4832ca4276be74c5007db6c2f3c67f5b15652623
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-11-11 18:01:40 +0700

    update face-compare: add result

--- assets/views/face-compare/face-compare.js
+++ assets/views/face-compare/face-compare.js
@@ -13,11 +13,22 @@
     _.$inject = ['$scope', 'FaceImageService'];
     function _($scope, FaceImageService) {
         let $ctrl = this;
-        $ctrl.$onInit = () => { };
+        $ctrl.$onInit = () => {
+            $scope.$watch('imageSource', () => {
+                if ($scope.imageSource) {
+                    $scope._imageSource = window.URL.createObjectURL($scope.imageSource[0]);
+                }
+            });
+            $scope.$watch('imageTarget', () => {
+                if ($scope.imageTarget) {
+                    $scope._imageTarget = window.URL.createObjectURL($scope.imageTarget[0]);
+                }
+            });
+        };
 
         $scope.upload = async () => {
-            const res = await FaceImageService.faceCompare({ imageSource: $scope.imageSource, imageTarget: $scope.imageTarget });
-            console.log(res);
+            $scope.result = await FaceImageService.faceCompare({ imageSource: $scope.imageSource, imageTarget: $scope.imageTarget });
+            $scope.$apply();
         };
     }
 })();
